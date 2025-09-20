42df1ad3025a12d7c11a2aa0ffaa894098e99042
Detroix23 <detroix23@gmail.com>
2025-09-20 17:25:53 +0200

    Even more UI, colors.

--- anagrams/modules/ui.py
+++ anagrams/modules/ui.py
@@ -8,6 +8,50 @@ import modules.anagrams as anagrams
 import modules.dictionnaries as dictionnaries
 import modules.loadings as loadings
 
+ART: dict[str, str] = {
+    "Title": """
+
+ _____                                                                            _____ 
+( ___ )                                                                          ( ___ )
+ |   |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|   | 
+ |   |                                                                            |   | 
+ |   |   ████████╗██████╗ ██╗   ██╗███████╗                                       |   | 
+ |   |   ╚══██╔══╝██╔══██╗██║   ██║██╔════╝                                       |   | 
+ |   |      ██║   ██████╔╝██║   ██║█████╗                                         |   | 
+ |   |      ██║   ██╔══██╗██║   ██║██╔══╝                                         |   | 
+ |   |      ██║   ██║  ██║╚██████╔╝███████╗                                       |   | 
+ |   |      ╚═╝   ╚═╝  ╚═╝ ╚═════╝ ╚══════╝                                       |   | 
+ |   |    █████╗ ███╗   ██╗ █████╗  ██████╗ ██████╗  █████╗ ███╗   ███╗███████╗   |   | 
+ |   |   ██╔══██╗████╗  ██║██╔══██╗██╔════╝ ██╔══██╗██╔══██╗████╗ ████║██╔════╝   |   | 
+ |   |   ███████║██╔██╗ ██║███████║██║  ███╗██████╔╝███████║██╔████╔██║███████╗   |   | 
+ |   |   ██╔══██║██║╚██╗██║██╔══██║██║   ██║██╔══██╗██╔══██║██║╚██╔╝██║╚════██║   |   | 
+ |   |   ██║  ██║██║ ╚████║██║  ██║╚██████╔╝██║  ██║██║  ██║██║ ╚═╝ ██║███████║   |   | 
+ |   |   ╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝   |   | 
+ |   |                                                                            |   | 
+ |___|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|___| 
+(_____)                                                                          (_____)
+""",
+    "Border1": """
+
+     ███ ███   ███ ███   ███ ███   ███ ███   ░░░ ░░░   ░░░ ░░░     
+   ███░███░  ███░███░  ███░███░  ███░███░  ███░███░  ░░░ ░░░ 
+  ░░░ ░░░   ░░░ ░░░   ░░░ ░░░   ░░░ ░░░   ░░░ ░░░                   
+ 
+"""
+}
+
+class Style:
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    FAIL = '\033[91m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    OKBLUE = '\033[94m'
+    HEADER = '\033[95m'
+    OKCYAN = '\033[96m'
+
+
 def main_ui() -> None:
     """
     Main function execute on run.
@@ -18,7 +62,7 @@ def main_ui() -> None:
     # Loading animations.
     loading_intersect: loadings.Spinner = loadings.spinners["Wave2"].__copy__()
     loading_intersect.prefix = "Searching dict: "
-    loading_intersect.multiple = 700
+    loading_intersect.multiple = 1000
     loading_intersect.more_counters("words")
 
     loading_combinations: loadings.Spinner = loadings.spinners["Wave2"].__copy__()
@@ -27,11 +71,18 @@ def main_ui() -> None:
     loading_combinations.more_counters("anagrams")
 
     # Main
-    print("# ANAGRAMS.")
-    print(f"Using dictionnary: {dictionnary_path}")
+    print(ART["Title"])
+    print(f" {Style.HEADER}By Detroix23, 2025.{Style.ENDC}")
+    print("  https://github.com/Detroix23/TrueAnagrams")
+    print("  CC-BY 4.0")
+    print()
+    print(f" {Style.OKCYAN}( Using dictionnary: {Style.BOLD}{dictionnary_path}{Style.ENDC}{Style.OKCYAN} ){Style.ENDC}")
+    print(f"{ART["Border1"]}")
+
     try:
         while True:
-            user_word: str = input("- Enter a word: ")
+            user_word: str = input(f"- Enter a word: {Style.BOLD}")
+            print(f"{Style.ENDC}", end="")
             if user_word:
                 user_anagrams: set[str] = anagrams.get_all_combinations(
                     user_word,
@@ -54,14 +105,14 @@ def main_ui() -> None:
                     ))
                 else:
                     print(f"=> Correct anagrams 0/{len(user_anagrams)}:")
-                    print("\tNo correct anagrams! ")
+                    print(f"\t{Style.FAIL}No correct anagrams!{Style.ENDC}")
 
                 print("")
                 loading_combinations.reset()
                 loading_intersect.reset()
 
     except KeyboardInterrupt:
-        print("\n(+) - User interrupt (Ctrl+C).")
+        print(f"\n{Style.ENDC}{Style.WARNING}(+) - User interrupt (Ctrl+C).{Style.ENDC}")
 
 
 def set_to_table(
@@ -70,7 +121,8 @@ def set_to_table(
     row_prefix: str = "\t",
     row_suffix: str = "",
     spacer: str = " ",
-    table_footer: str = "─"
+    table_footer: str = "─",
+    color: str = Style.OKGREEN
 ) -> str:
     """
     Return a formatted string of row-col table.
@@ -83,7 +135,7 @@ def set_to_table(
         if char_count > max_per_col and len(element) <= max_per_col:
             table += f"{row_suffix}\n{row_prefix}"
         else:
-            table += f"{spacer}{element}"
+            table += f"{spacer}{color}{element}{Style.ENDC}"
 
     if table_footer:
         table += f"\n{row_prefix}{table_footer * max_per_col}"
